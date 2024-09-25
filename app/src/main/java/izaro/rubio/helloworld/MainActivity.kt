edf789a15cfcacd47f34bc6cd6b59057d741c5f0
izaro.rubio <izaro.rubio@euneiz.com>
2024-09-25 17:27:13 +0200

    First changes

--- app/src/main/java/izaro/rubio/helloworld/MainActivity.kt
+++ app/src/main/java/izaro/rubio/helloworld/MainActivity.kt
@@ -1,6 +1,7 @@
 package izaro.rubio.helloworld
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Log.d("HelloWorld", "IZARO onCreate" )
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("HelloWorld", "IZARO onResume" )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("HelloWorld", "IZARO onStart" )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("HelloWorld", "IZARO onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("HelloWorld", "IZARO onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("HelloWorld", "IZARO onDestroy")
     }
 }
