58c9978a27e18d25775517568d88fc813e24b0c9
izaro.rubio <izaro.rubio@euneiz.com>
2024-10-02 17:39:18 +0200

    Layouts and binding

--- app/src/main/java/izaro/rubio/helloworld/MainActivity.kt
+++ app/src/main/java/izaro/rubio/helloworld/MainActivity.kt
@@ -6,12 +6,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import izaro.rubio.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("HelloWorld", "IZARO onCreate" )
 
+        binding.helloworldView.text= "hola a todos"
 
     }
 
